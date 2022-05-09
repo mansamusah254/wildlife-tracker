88a6cba5e9b7f82e0e6cb3112c91ed1e2db97e99
mansamusah254 <mansamusah830@gmail.com>
2022-05-09 16:24:35 +0300

    adjustments

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,6 +1,7 @@
 import org.sql2o.Sql2o;
 
 public class DB {
+
 //        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wild_track", "postgres", "password");
-        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-174-129-33-75.compute-1.amazonaws.com:5432/dfki83t6hkdabm", "lxzyxrnzqubzas", "54acab5be4c3552497e0de92d734ee4c789d1cd828c648e8a51470908de26647");
+        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-4-104-184.compute-1.amazonaws.com:5432/d4nqnvk43ts128","wrfjwpkhufxyaf","d422ab6da4156c4bf730ee836b00d41b9a9ce07ed2171bd432b9c227345aa2bb");
 }
